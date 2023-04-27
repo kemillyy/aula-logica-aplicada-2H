e73aa64f75af9bae7f18f2d2b0e7c2a2d94b1c5e
Kemilly Almeida Moura <rm87428@estudante.fieb.edu.br>
2023-04-27 08:07:05 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int contador =0;
+        // laço de repetoção
+        while (contador < 10) {
+            System.out.println("Contador =" + contador);
+            // incremento
+            contador = contador + 1;
+        }
+
+        // laço para faça
+        for(int x=0;  x < 10; x++){
+            System.out.println("Contador = " + x);
+        }
+
+        // lsço faça enquanto
+        int cont = 1;
+        do{
+           // processo
+            System.out.println(cont);
+            // incremento
+            cont++;
+        }while (cont <= 10);
+    }
+}
